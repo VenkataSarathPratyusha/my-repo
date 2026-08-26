26c613ca43c6d379063f46e2b415d3d8df7d8f70
pretty <pretty@devops.com>
2026-08-26 17:47:42 +0000

    java file added

---
+++ app.java
@@ -0,0 +1,7 @@
+// The class name must exactly match the filename
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints the text to the console
+        System.out.println("Hello, World!"); 
+    }
+}
